3e1f3b1fd05fdf987a274f15d2c6dd5ea9ef73d4
egeuzma <egeuzma@Lenovo-PC>
2020-04-26 22:35:09 +0300

    En son olarak firma eklendi database kuruldu update insert methodları yapıldı firma tableview kuruldu ve database ile conneciton sağlandı.tahminimce artık proje bitmek üzere

--- src/proje/ManyetikRaporController.java
+++ src/proje/ManyetikRaporController.java
@@ -5,16 +5,23 @@
  */
 package proje;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import javax.imageio.ImageIO;
 
 /**
  * FXML Controller class
@@ -23,12 +30,12 @@ import javafx.stage.Stage;
  */
 public class ManyetikRaporController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML private ImageView image1;
+    @FXML private ImageView image2;
+    private File imageFile;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
      public void GoBack(ActionEvent event) throws IOException{
        Parent Goback = FXMLLoader.load(getClass().getResource("RaporOlustur.fxml"));
--- src/proje/RadyografikRaporuController.java
+++ src/proje/RadyografikRaporuController.java
@@ -9,11 +9,14 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 /**
@@ -23,9 +26,7 @@ import javafx.stage.Stage;
  */
 public class RadyografikRaporuController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+     @FXML private ImageView imageview;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
